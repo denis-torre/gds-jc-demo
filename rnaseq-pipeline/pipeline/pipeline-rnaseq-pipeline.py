ef30fa11182b8613789fa6509da9d2fc8facae26
denis-torre <denis.torre@icahn.mssm.edu>
2021-03-02 11:47:47 -0500

    Initial upload

---
+++ rnaseq-pipeline/pipeline/pipeline-rnaseq-pipeline.py
@@ -0,0 +1,101 @@
+#################################################################
+#################################################################
+############### Demo RNA-Seq Pipeline ################
+#################################################################
+#################################################################
+##### Author: Denis Torre
+##### Affiliation: Guccione Laboratory,
+##### Icahn School of Medicine at Mount Sinai
+
+#############################################
+########## 1. Load libraries
+#############################################
+##### 1. Python modules #####
+from ruffus import *
+import ruffus.cmdline as cmdline
+import sys
+import os
+import json
+import glob
+import pandas as pd
+import numpy as np
+# from rpy2.robjects import r, pandas2ri
+# pandas2ri.activate()
+
+##### 2. LSF #####
+# 2.1 Import
+sys.path.append('/hpc/users/torred23/pipelines/support')
+import lsf
+
+# 2.2 Default parameters
+r_source = 'pipeline/scripts/rnaseq-pipeline.R'
+py_source = 'pipeline/scripts/RnaseqPipeline.py'
+P = 'acc_GuccioneLab'
+q = 'express'
+W = '00:30'
+GB = 5
+n = 1
+mkdir_val = True
+
+# 2.3 Wrappers
+# CMD
+def run_job(cmd_str, outfile, W = W, GB = GB, n = n, q = q, **kwargs):
+	lsf.run_job(cmd_str, outfile, P=P, W = W, GB = GB, n = n, q = q, mkdir=mkdir_val, **kwargs)
+
+# R
+def run_r_job(func_name, func_input, outfile, W = W, GB = GB, n = n, q = q, **kwargs):
+	lsf.run_r_job(func_name, func_input, outfile, r_source=r_source, P=P, W = W, GB = GB, n = n, q = q, mkdir=mkdir_val, **kwargs)
+
+# Py
+def run_py_job(func_name, func_input, outfile, W = W, GB = GB, n = n, q = q, **kwargs):
+	lsf.run_py_job(func_name, func_input, outfile, py_source=py_source, P=P, W = W, GB = GB, n = n, q = q, mkdir=mkdir_val, **kwargs)
+
+##### 3. Custom script imports #####
+# 3.1 Python
+#sys.path.append('pipeline/scripts')
+#import RnaseqPipeline as P
+
+# 3.2 R
+# r.source(r_source)
+
+#############################################
+########## 2. General Setup
+#############################################
+##### 1. Pipeline running #####
+# Pipeline args
+options = cmdline.get_argparse().parse_args()
+
+##### 2. Genome indices #####
+# Open JSON
+
+##### 3. Variables #####
+
+#######################################################
+#######################################################
+########## S1. 
+#######################################################
+#######################################################
+
+#############################################
+########## 1. 
+#############################################
+
+
+#######################################################
+#######################################################
+########## S. 
+#######################################################
+#######################################################
+
+#############################################
+########## . 
+#############################################
+
+##################################################
+##################################################
+########## Run pipeline
+##################################################
+##################################################
+if __name__ == '__main__':
+	cmdline.run(options)
+print('Done!')
