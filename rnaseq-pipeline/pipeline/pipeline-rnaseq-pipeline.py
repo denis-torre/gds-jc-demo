ca829f3d6468d40f82d829fea3acc218fbe75f5e
denis-torre <denis.torre@icahn.mssm.edu>
2021-03-02 13:10:04 -0500

    Gene count reading

--- rnaseq-pipeline/pipeline/pipeline-rnaseq-pipeline.py
+++ rnaseq-pipeline/pipeline/pipeline-rnaseq-pipeline.py
@@ -1,97 +1,69 @@
 #################################################################
 #################################################################
-############### Demo RNA-Seq Pipeline ################
+############### Demo RNA-Seq Pipeline ###########################
 #################################################################
 #################################################################
 ##### Author: Denis Torre
-##### Affiliation: Guccione Laboratory,
 ##### Icahn School of Medicine at Mount Sinai
 
 #############################################
-########## 1. Load libraries
+########## 1. Pipeline setup
 #############################################
-##### 1. Python modules #####
+### 1. Python
 from ruffus import *
 import ruffus.cmdline as cmdline
-import sys
-import os
-import json
-import glob
+import sys, os
 import pandas as pd
 import numpy as np
-# from rpy2.robjects import r, pandas2ri
-# pandas2ri.activate()
 
-##### 2. LSF #####
-# 2.1 Import
-sys.path.append('/hpc/users/torred23/pipelines/support')
-import lsf
+### 2. Custom
+sys.path.append('pipeline/scripts')
+import job_manager
 
-# 2.2 Default parameters
-r_source = 'pipeline/scripts/rnaseq-pipeline.R'
-py_source = 'pipeline/scripts/RnaseqPipeline.py'
+### 3. Function to run R jobs
+# Minerva queue to use
+# NOTE: insert your lab's queue to run jobs yourself!
 P = 'acc_GuccioneLab'
-q = 'express'
-W = '00:30'
-GB = 5
-n = 1
-mkdir_val = True
-
-# 2.3 Wrappers
-# CMD
-def run_job(cmd_str, outfile, W = W, GB = GB, n = n, q = q, **kwargs):
-	lsf.run_job(cmd_str, outfile, P=P, W = W, GB = GB, n = n, q = q, mkdir=mkdir_val, **kwargs)
 
 # R
-def run_r_job(func_name, func_input, outfile, W = W, GB = GB, n = n, q = q, **kwargs):
-	lsf.run_r_job(func_name, func_input, outfile, r_source=r_source, P=P, W = W, GB = GB, n = n, q = q, mkdir=mkdir_val, **kwargs)
-
-# Py
-def run_py_job(func_name, func_input, outfile, W = W, GB = GB, n = n, q = q, **kwargs):
-	lsf.run_py_job(func_name, func_input, outfile, py_source=py_source, P=P, W = W, GB = GB, n = n, q = q, mkdir=mkdir_val, **kwargs)
-
-##### 3. Custom script imports #####
-# 3.1 Python
-#sys.path.append('pipeline/scripts')
-#import RnaseqPipeline as P
-
-# 3.2 R
-# r.source(r_source)
-
-#############################################
-########## 2. General Setup
-#############################################
-##### 1. Pipeline running #####
-# Pipeline args
-options = cmdline.get_argparse().parse_args()
-
+def run_r_job(func_name, func_input, outfile, W = '00:30', GB = 5, n = 1, q = 'express', **kwargs):
+	job_manager.run_r_job(func_name, func_input, outfile, r_source='pipeline/scripts/rnaseq-pipeline.R', P=P, W = W, GB = GB, n = n, q = q, mkdir=True, **kwargs)
 
 #######################################################
 #######################################################
-########## S1. 
+########## Pipeline steps
 #######################################################
 #######################################################
 
 #############################################
-########## 1. 
+########## Step 1. Read gene counts
 #############################################
+# Input: gene expression file in TSV format
+# Output: SummarizedExperiment file in .rda file
+# Type of operation: 1-to-1
+# Ruffus decorator used: transform
 
+# Input file path
+counts_file = 'data/test.tsv'
 
-#######################################################
-#######################################################
-########## S. 
-#######################################################
-#######################################################
+@transform(counts_file,
+		   suffix('.tsv'),
+		   '.rda')
 
-#############################################
-########## . 
-#############################################
+def readGeneCounts(infile, outfile):
+
+	# Run
+	run_r_job('read_gene_counts', infile, outfile, run_locally=True)
 
 ##################################################
 ##################################################
 ########## Run pipeline
 ##################################################
 ##################################################
+# Get options specified from commandline
+options = cmdline.get_argparse().parse_args()
+
+# Run indicated steps
 if __name__ == '__main__':
 	cmdline.run(options)
 print('Done!')
--- rnaseq-pipeline/pipeline/scripts/rnaseq-pipeline.R
+++ rnaseq-pipeline/pipeline/scripts/rnaseq-pipeline.R
@@ -1,33 +1,30 @@
 #################################################################
 #################################################################
-############### Demo RNA-Seq Pipeline - R Support #################
+############### Demo RNA-Seq Pipeline - R Support ###############
 #################################################################
 #################################################################
+# Load libraries
+suppressPackageStartupMessages(require(DESeq2))
+suppressPackageStartupMessages(require(data.table))
+suppressPackageStartupMessages(require(dplyr))
+suppressPackageStartupMessages(require(tibble))
 
 #############################################
-########## 1. Load libraries
-#############################################
-##### 1. General support #####
-
-##### 2. Other libraries #####
-
-#######################################################
-#######################################################
-########## S1. 
-#######################################################
-#######################################################
-
-#############################################
-########## 1. 
+########## Step 1. Read gene counts
 #############################################
 
+read_gene_counts <- function(infile, outfile) {
 
-#######################################################
-#######################################################
-########## S. 
-#######################################################
-#######################################################
+    # Read counts
+    count_matrix <- fread(infile) %>% column_to_rownames('gene_id') %>% as.matrix
 
-#############################################
-########## . 
-#############################################
+    # Get sample info
+    metadata_dataframe <- data.frame(sample_name=colnames(count_matrix)) %>% mutate(treatment=gsub('(.*)_.', '\\1', sample_name)) %>% column_to_rownames('sample_name')
+
+    # Create DDS
+    dds <- DESeqDataSetFromMatrix(countData = count_matrix, colData = metadata_dataframe, design = ~treatment)
+
+    # save
+    save(dds, file=outfile)
+
+}
